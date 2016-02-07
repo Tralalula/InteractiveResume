16337cb55a13deff9fba4c511e69d947d75b8a09
Tobias <qenzoz@gmail.com>
2016-02-07 16:07:14 +0100

    Add education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -25,6 +25,68 @@ bio = {
   bioPic: "https://www.placecage.com/c/387/315"
 };
 
+education = {
+  "schools": [
+    {
+      "name": "Baconversity",
+      "location": "Denmark, København",
+      "degree": "Ph.D.",
+      "majors": [
+        "Quantum Mechanics",
+        "Cryptozoology",
+        "Bagels"
+      ],
+      "dates": "2027",
+      "url": "N/A"
+    },
+    {
+      "name": "University of Tabletop",
+      "location": "United States, California",
+      "degree": "Ph.Deluxe",
+      "majors": [
+        "Terra Mystica",
+        "Mage Knight",
+        "Small World",
+        "Munchkin: Apocalypse"
+      ],
+      dates: "2032",
+      "url": "N/A"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Intro to HTML and CSS",
+      "school": "Udacity",
+      "date": "2016",
+      "url": "https://www.udacity.com/courses/ud304"
+    },
+    {
+      "title": "Responsive Web Design Fundamentals",
+      "school": "Udacity",
+      "date": "2016",
+      "url": "https://www.udacity.com/courses/ud893"
+    },
+    {
+      "title": "Responsive Images",
+      "school": "Udacity",
+      "date": "2016",
+      "url": "https://www.udacity.com/courses/ud882"
+    },
+    {
+      "title": "JavaScript Basics",
+      "school": "Udacity",
+      "date": "2016",
+      "url": "https://www.udacity.com/courses/ud804"
+    },
+    {
+      "title": "Intro to jQuery",
+      "school": "Udacity",
+      "date": "2016",
+      "url": "https://www.udacity.com/courses/ud245"
+    }
+  ]
+};
+
 bio.display = function () {
   var headerId = "#header";
   $(headerId).prepend(HTMLheaderRole.replace(data, bio.role));
@@ -57,4 +119,35 @@ bio.display = function () {
   }
 };
 
+education.display = function () {
+  var educationId = "#education";
+  var educationEntryClass = ".education-entry:last";
+  if (education.schools.length > 0) {
+    $(educationId).append(HTMLschoolStart);
+    for (var school in education.schools) {
+      $(educationEntryClass).append(
+          HTMLschoolName.replace(data, education.schools[school].name) +
+          HTMLschoolDegree.replace(data, education.schools[school].degree)
+      );
+      $(educationEntryClass).append(HTMLschoolDates.replace(data, education.schools[school].dates));
+      $(educationEntryClass).append(HTMLschoolLocation.replace(data, education.schools[school].location));
+      $(educationEntryClass).append(HTMLschoolMajor.replace(data, education.schools[school].majors));
+    }
+  }
+
+  if (education.onlineCourses.length > 0) {
+    $(educationId).append(HTMLonlineClasses);
+    for (var course in education.onlineCourses) {
+      $(educationId).append(HTMLschoolStart);
+      $(educationEntryClass).append(
+          HTMLonlineTitle.replace(data, education.onlineCourses[course].title) +
+          HTMLonlineSchool.replace(data, education.onlineCourses[course].school)
+      );
+      $(educationEntryClass).append(HTMLonlineDates.replace(data, education.onlineCourses[course].date));
+      $(educationEntryClass).append(HTMLonlineURL.replace(data, education.onlineCourses[course].url) + "<br>");
+    }
+  }
+};
+
 bio.display();
+education.display();
